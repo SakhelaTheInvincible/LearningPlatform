b87d4c94fea23eccca568a899bf6e527662ddc4f
SakhelaTheInvincible <135421822+SakhelaTheInvincible@users.noreply.github.com>
2025-04-01 20:34:26 +0400

    updated structure of project

---
+++ frontend/src/app/assessment/page.tsx
@@ -0,0 +1,7 @@
+export default function AssessmentPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Assessment</h1>
+    </main>
+  );
+} 
